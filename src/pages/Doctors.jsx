68bd54d9c44fbe229efe6191b790051b8c859cf6
Abu-Farhad <abufarhadsardar91@gmail.com>
2025-10-04 00:06:29 +0530

    update

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -36,7 +36,7 @@ export default function Doctors() {
 
         <button onClick={()=>setShowFilter(prev=>!prev)} className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter && 'bg-primary text-white'}`}>Filters</button>
 
-        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter?'':'sm-flex hidden'}`}>
+        <div className={`flex-col gap-0 text-sm text-gray-600 ${showFilter?'':'sm:flex hidden'}`}>
           <p
             onClick={() =>{speciality==='Gynecologist'? navigate('/doctors'):navigate("/doctors/Gynecologist")} }
             className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality==='Gynecologist'?'bg-indigo-100 text-black':''}`}
